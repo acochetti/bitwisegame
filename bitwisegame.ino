9b7c7bff2ddf9266aaf20d04cf03fd8f953576ca
Anthony M Cochetti <sarksus@openmailbox.org>
2015-10-08 11:11:10 -0400

    replaced spaghetti code with arrays and for loops

--- bitwisegame.ino
+++ bitwisegame.ino
@@ -10,68 +10,43 @@
 #define OLED_RESET 13
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
-const int buttonPin1 = 2;
-int buttonState1 = 0;
-
-const int buttonPin2 = 3;
-int buttonState2 = 0;
-
-const int buttonPin3 = 4;
-int buttonState3 = 0;
-
-const int buttonPin4 = 5;
-int buttonState4 = 0;
-String str1, str2, str3, str4;
+const int buttonPins[8] = {6, 7, 8, 14, 5, 4, 3, 2};
+int buttonStates[8] = { };
+String binaryDigits[8];
 
 void setup() {
   Serial.begin(9600);
-
   display.begin(SSD1306_SWITCHCAPVCC);
   display.clearDisplay();
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
-  pinMode(buttonPin3, INPUT);
-  pinMode(buttonPin4, INPUT);
+
+  for(int x = 0; x < 8; x++) {
+    pinMode(buttonPins[x], INPUT);
+  }
   
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  //display.print("donuts");
-  //display.display();
-  
-
 }
 
 void loop() {
-  
-  buttonState1 = digitalRead(buttonPin1);
-  buttonState2 = digitalRead(buttonPin2);
-  buttonState3 = digitalRead(buttonPin3);
-  buttonState4 = digitalRead(buttonPin4);
-  
   display.setCursor(0,0);
   
-  if (buttonState1 == HIGH) {
-    str1 = "0";
-  } else {
-    str1 = "1";
+  for (int x = 0; x < 8; x++) {
+    buttonStates[x] = digitalRead(buttonPins[x]);
+    
+    if (buttonStates[x] == HIGH) {
+      binaryDigits[x] = "0";
+    } else {
+      binaryDigits[x] = "1";
+    }
+    
   }
-  if (buttonState2 == HIGH) {
-    str2 = "0";
-  } else {
-    str2 = "1";
-  }
-  if (buttonState3 == HIGH) {
-    str3 = "0";
-  } else {
-    str3 = "1";
-  }
-  if (buttonState4 == HIGH) {
-    str4 = "0";
-  } else {
-    str4 = "1";
-  }
+    
   display.clearDisplay();
-  display.print(str1 + str2 + str3 + str4);
+  
+  for (int x = 0; x < 8; x++) {
+    display.print(binaryDigits[x]);
+  }
+  
   display.display();
   
 }
