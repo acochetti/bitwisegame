53f17456016f5aa992d09cd197badd4ebeca0136
Anthony M Cochetti <sarksus@openmailbox.org>
2015-10-13 09:39:22 -0400

    generate integer versions of binary numbers from arrays

--- bitwisegame.ino
+++ bitwisegame.ino
@@ -12,17 +12,46 @@ Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 const int buttonPins[8] = {6, 7, 8, 14, 5, 4, 3, 2};
 int buttonStates[8] = { };
-String binaryDigits[8];
+int binaryDigits[8], firstBinary[8], secretBinary[8], resultBinary[8];
+int x, operation, mask;
+String opString;
+unsigned int firstInt = 0, secretInt = 0, resultsInt = 0; //these will be generated from the arrays 
+                                                          //and used for the actual bitwise arithmetic 
+                                                          //so that we can reduce our reliance on for loops
 
 void setup() {
   Serial.begin(9600);
+  randomSeed(analogRead(23));
   display.begin(SSD1306_SWITCHCAPVCC);
   display.clearDisplay();
 
-  for(int x = 0; x < 8; x++) {
+  //setup loop
+  for(x = 0; x < 8; x++) {
+    mask = 1;
     pinMode(buttonPins[x], INPUT);
+    
+    firstBinary[x] = random(2); //assigns either a 0 or 1 to each index
+    if (firstBinary[x]) {
+      mask = mask << x;
+      firstInt = firstInt | mask;
+    }
+    
+    secretBinary[x] = random(2);
+    if (secretBinary[x]){
+      mask = mask << x;
+      secretInt = secretInt | mask;
+    }
   }
   
+  operation = random(2); //assigns either a 0, representing AND, or a 1, representing OR, to the variable operation
+  if (operation) {
+    opString = "&";
+  } else {
+    opString = "|";
+  }
+
+  resultsInt = firstInt | secretInt;
+
   display.setTextSize(1);
   display.setTextColor(WHITE);
 }
@@ -30,22 +59,31 @@ void setup() {
 void loop() {
   display.setCursor(0,0);
   
-  for (int x = 0; x < 8; x++) {
+  for (x = 0; x < 8; x++) {
     buttonStates[x] = digitalRead(buttonPins[x]);
     
     if (buttonStates[x] == HIGH) {
-      binaryDigits[x] = "0";
+      binaryDigits[x] = 0;
     } else {
-      binaryDigits[x] = "1";
+      binaryDigits[x] = 1;
     }
     
   }
     
   display.clearDisplay();
+
+  for (x = 7; x >=0 ; x--) {
+    display.print(firstBinary[x]);
+  }
+
+  display.println();
   
-  for (int x = 0; x < 8; x++) {
+  for (x = 0; x < 8; x++) {
     display.print(binaryDigits[x]);
   }
+
+  display.println(opString);
+  display.print(resultsInt); //this does not yet display in binary
   
   display.display();
   
