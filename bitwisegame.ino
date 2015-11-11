580eb40bdbae91a8e6e609f38adce51743ab479d
Anthony M Cochetti <sarksus@openmailbox.org>
2015-11-11 17:19:33 -0500

    completed basic functionality for AND/OR operations

--- bitwisegame.ino
+++ bitwisegame.ino
@@ -12,12 +12,15 @@ Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 const int buttonPins[8] = {6, 7, 8, 14, 5, 4, 3, 2};
 int buttonStates[8] = { };
-int binaryDigits[8], firstBinary[8], secretBinary[8], resultBinary[8];
+int answerDigits[8], firstBinary[8], secondBinary[8], resultBinary[8];
 int x, operation, mask;
 String opString;
-unsigned int firstInt = 0, secretInt = 0, resultsInt = 0; //these will be generated from the arrays 
+
+unsigned long startTime, elapsedTime;
+//unsigned int firstInt = 0, secretInt = 0, resultsInt = 0; //these will be generated from the arrays 
                                                           //and used for the actual bitwise arithmetic 
                                                           //so that we can reduce our reliance on for loops
+                                                          //not being used yet
 
 void setup() {
   Serial.begin(9600);
@@ -27,64 +30,112 @@ void setup() {
 
   //setup loop
   for(x = 0; x < 8; x++) {
-    mask = 1;
+    //mask = 1;
     pinMode(buttonPins[x], INPUT);
     
     firstBinary[x] = random(2); //assigns either a 0 or 1 to each index
-    if (firstBinary[x]) {
+    /*if (firstBinary[x]) {
       mask = mask << x;
       firstInt = firstInt | mask;
-    }
+    }*/
     
-    secretBinary[x] = random(2);
-    if (secretBinary[x]){
+    secondBinary[x] = random(2);
+    /*if (secretBinary[x]){
       mask = mask << x;
       secretInt = secretInt | mask;
-    }
+    }*/
   }
   
   operation = random(2); //assigns either a 0, representing AND, or a 1, representing OR, to the variable operation
   if (operation) {
-    opString = "&";
-  } else {
     opString = "|";
+
+    for(x = 0; x < 8; x++) {
+      if(firstBinary[x] || secondBinary[x]) {
+        resultBinary[x] = 1;
+      } else {
+        resultBinary[x] = 0;
+      }
+    }
+  } else {
+    opString = "&";
+
+    for(x = 0; x < 8; x++) {
+      if(firstBinary[x] && secondBinary[x]) {
+        resultBinary[x] = 1;
+      } else {
+        resultBinary[x] = 0;
+      }
+    }
   }
 
-  resultsInt = firstInt | secretInt;
+  //resultsInt = firstInt | secretInt;  //we'll use arrays and for loops first to get the functionality in place
 
   display.setTextSize(1);
   display.setTextColor(WHITE);
 }
 
 void loop() {
+  startTime = millis();
   display.setCursor(0,0);
+
+  
   
   for (x = 0; x < 8; x++) {
     buttonStates[x] = digitalRead(buttonPins[x]);
     
     if (buttonStates[x] == HIGH) {
-      binaryDigits[x] = 0;
+      answerDigits[x] = 0;
     } else {
-      binaryDigits[x] = 1;
+      answerDigits[x] = 1;
     }
     
   }
     
   display.clearDisplay();
 
-  for (x = 7; x >=0 ; x--) {
+  for(x = 0; x < 8; x++) {
     display.print(firstBinary[x]);
   }
 
   display.println();
-  
-  for (x = 0; x < 8; x++) {
-    display.print(binaryDigits[x]);
+
+  for(x = 0; x < 8; x++) {
+    display.print(secondBinary[x]);
   }
 
   display.println(opString);
-  display.print(resultsInt); //this does not yet display in binary
+
+  for (x = 0; x < 8; x++) {
+    display.print(answerDigits[x]);
+  }  
+
+  if (startTime >= 20000) {
+    if (binaryCompare(answerDigits, resultBinary) == 0) {
+      display.println("Correct!");
+    } else {
+      display.println("Incorrect!");
+    }
+  } else {
+      display.println(startTime / 1000);
+  }
+
+  
   
   display.display();
   
+  
 }
+
+int binaryCompare(int *answer, int *result) {
+  int x;
+  
+  for(x = 0; x < 7; x++) {
+    if(answer[x] != result[x]) {
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
